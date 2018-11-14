f2fdcdb6bf1102d0291d04b871beff450bf7acf7
Sakkyoku-Sha <RyanAnderson0147@gmail.com>
2018-11-14 15:36:50 -0700

    Partial Assignments now uses HashMap

--- ParseData/Partial_Assignments.java
+++ ParseData/Partial_Assignments.java
@@ -3,75 +3,53 @@ package ParseData;
 import Slot_Occupant.Slot_Occupant;
 import javafx.util.Pair;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Vector;
+
+import java.util.HashMap;
 
 public class Partial_Assignments {
 
-    private HashSet<Pair<Slot_Occupant, Slot>> Partial_Entries;
+    //This is a fairly lazy way to do this, but is effectively what a bimap is, except less space efficient.
+    //This is quite a bit faster than the hashset however;
+    private HashMap<Slot,Slot_Occupant> Partial_Entries_S_SO;
+    private HashMap<Slot_Occupant,Slot> Partial_Entries_SO_S;
 
     public Partial_Assignments(){
-        Partial_Entries = new HashSet<>();
+        Partial_Entries_S_SO = new HashMap<>();
+        Partial_Entries_SO_S = new HashMap<>();
     }
-
 
     //Gets the Occupant for a partial assignment, returns null if no slot occupant exists.
     public Slot_Occupant getOccupant(Slot a){
+       return Partial_Entries_S_SO.get(a);
+    }
 
-        Iterator<Pair<Slot_Occupant, Slot>> iter =  Partial_Entries.iterator();
-
-        while(iter.hasNext()){
-
-            Pair<Slot_Occupant, Slot> temp = iter.next();
-
-            if(a.equals(temp.getValue())){
-                return temp.getKey();
-            }
-        }
-
-        return null;
+    public Slot getSlot(Slot_Occupant a){
+        return Partial_Entries_SO_S.get(a);
     }
 
     //Gets the slot for a partial assignment, returns null if no slot occupant exists.
-    public Slot getSlot(Slot_Occupant a){
-
-        Iterator<Pair<Slot_Occupant, Slot>> iter =  Partial_Entries.iterator();
-
-        while(iter.hasNext()){
-
-            Pair<Slot_Occupant, Slot> temp = iter.next();
-
-            if(a.equals(temp.getKey())){
-                return temp.getValue();
-            }
-        }
-
-        return null;
-
+    public boolean isAssigned(Slot_Occupant a){
+       return Partial_Entries_SO_S.containsKey(a);
     }
 
-    //Note this won't be in any particular order, returns an array of pairs of partial assignments.
-    public Pair<Slot_Occupant, Slot>[] getAll(){
-
-        //This might need to be checked
-        return (Pair<Slot_Occupant, Slot>[])Partial_Entries.toArray();
-
+    public boolean isAssigned(Slot a){
+        return Partial_Entries_S_SO.containsKey(a);
     }
+
     public boolean isPartialEntry(Slot_Occupant a, Slot b){
-        return this.Partial_Entries.contains(newPair(a,b));
+        return (this.Partial_Entries_SO_S.get(a) == b);
     }
 
-    public boolean addEntry(Slot_Occupant a, Slot b){
-        return this.Partial_Entries.add(newPair(a,b));
+    public void addEntry(Slot_Occupant a, Slot b){
+        Partial_Entries_SO_S.put(a,b);
+        Partial_Entries_S_SO.put(b,a);
     }
 
     public boolean removeEntry(Slot_Occupant a, Slot b){
-        return this.Partial_Entries.remove(newPair(a,b));
-    }
+        boolean s = true;
+        s = Partial_Entries_SO_S.remove(a,b);
+        s = Partial_Entries_S_SO.remove(b,a);
 
-    private Pair<Slot_Occupant,Slot> newPair(Slot_Occupant a, Slot b){
-        return new Pair<Slot_Occupant, Slot>(a,b);
+        return s;
     }
-
 }
