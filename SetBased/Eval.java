ccc22fc24a2de13c00c738643ea35c056d59d8c0
bader-waseem <25441886+bader-waseem@users.noreply.github.com>
2018-12-02 14:37:09 -0700

    Added evalMinFill implementation

--- SetBased/Eval.java
+++ SetBased/Eval.java
@@ -82,11 +82,35 @@ public class Eval {
 
 
 
-    public int evalMinfilled(Map<Slot_Occupant, Slot> solution) {
-        // The penalty value that will be returned
+     // Counts the amount of times that the given solution does not meet the
+  // coursemin soft constraint
+  public int evalMinfilled(Map<Slot_Occupant, Slot> solution)) {
+      // The penalty value that will be returned
+      int pen = 0;
+      // Counts the amount of courses that are in a particular slot
+      int courseCounter;
 
-        return 0;
-    }
+      Set<Slot_Occupant> keys = solution.keySet();
+      Slot_Occupant[] allOccupants = keys.toArray();
+
+      Collection<Slot> values = solution.values();
+      Slot[] allSlots = values.toArray();
+
+    // Checks each slot and see how mnay courses occupy that slot
+      for (int slot = 0; slot < allSlots.length; slot++) {
+          courseCounter = 0;
+          for (int occupant = 0; occupant < allOccupants.length; occupant++) {
+              if (allSlots[slot].equals(solution.get(allOccupants[occupant]))) {
+                  courseCounter++;
+              }
+          }
+          if (courseCounter < allSlots[slot].min) {
+              pen++;
+          }
+      }
+
+      return pen;
+  }
 
 
 
